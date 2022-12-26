603a84229c00abb6cb6afe65c1d85b13cc4c3dba
DhioAWP <dhioanugerah0@gmail.com>
2022-12-26 17:26:33 +0700

    create public void insert

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class Program
+    class node
     {
-        class node
-        {
-            public string info;
-            public node lchild;
-            public node rchild;
+        public string info;
+        public node lchild;
+        public node rchild;
 
-            public node(string i, node l, node r)
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
+    class BinarySeacrhTree
+    {
+        public node ROOT;
+        public BinarySeacrhTree()
+        {
+            ROOT = null;
+        }
+        public void insert(string element)
+        {
+            node tmp, parent = null, currentnode = null;
+            find(element, ref parent, ref currentnode);
+            if (currentnode != null)
             {
-                info = i;
-                lchild = l;
-                rchild = r;
+                Console.WriteLine("Duplicate words not allowed");
+                return;
             }
         }
-
         static void Main(string[] args)
         {
         }
